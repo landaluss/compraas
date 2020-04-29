fcbc03c4d5f6e4920e6159d4af604c5a427a6ecf
landaluss <landaluss@gmail.com>
2020-04-29 17:02:59 +0200

    HTTP Request varias

--- app/src/main/java/com/chema/myapplication/PrincipalActivity.java
+++ app/src/main/java/com/chema/myapplication/PrincipalActivity.java
@@ -21,6 +21,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.NetworkError;
 import com.android.volley.NoConnectionError;
 import com.android.volley.ParseError;
@@ -215,6 +216,10 @@ public class PrincipalActivity extends AppCompatActivity {
                 }
             }
         });
+        jsonRequestLogin.setRetryPolicy(new DefaultRetryPolicy(
+                DefaultRetryPolicy.DEFAULT_TIMEOUT_MS * 5,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         fRequestQueue.add(jsonRequestLogin);
 
     }
